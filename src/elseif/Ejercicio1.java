df442ba6d6e5f131bdd455958c5ee4b9b83f19b7
Gonzalo <Gonzalo@DESKTOP-76ILFO1>
2025-10-18 16:06:25 +0200

    18/10/25

--- src/elseif/Ejercicio1.java
+++ src/elseif/Ejercicio1.java
@@ -1,5 +1,28 @@
 package elseif;
 
-public class Ejercicio1 {
+import java.util.Scanner;
 
+public class Ejercicio1 {
+	public static void main(String[] args) {
+	//Declaramos la variable.
+	int numero;
+
+	//Creo un escaner pa almacenar valores
+	Scanner reader = new Scanner (System.in);
+	
+	//Realizamos la pregunta al usuario.
+	System.out.println("Introduzca un número: ");
+	
+	numero = reader.nextInt();
+	
+	//Realizamos el condicional.
+			if (numero % 2 == 0) {
+					System.out.println("El número introducido es par.");
+				}		 else {
+						System.out.println("El número introducido es impar.");
+					}
+				
+	//Cerramos el scanner.
+	reader.close();
+}
 }
--- src/elseif/Ejercicio2.java
+++ src/elseif/Ejercicio2.java
@@ -15,16 +15,16 @@ public class Ejercicio2 {
 	Scanner reader = new Scanner (System.in);
 	
 	//Imprimimos la pregunta .
-	System.out.println("Introduzca un número decimal: ");
+	System.out.println("Introduzca un numero: ");
 		
 		//Guardamos la informacion
 		numero = reader.nextDouble();
 		
 		//HAcemos un if para.
 		if (numero > -1 && numero < 1 && numero != 0) {
-			System.out.println("El número es casi-cero.");
+			System.out.println("es casi cero");
 		} else {
-			System.out.println("El número NO es casi-cero.");
+			System.out.println("no es casi cero");
 		}
 		
 		//Cerramos el scannner.
