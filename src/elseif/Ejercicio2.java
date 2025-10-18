d4c644a00e61ada27805ea6693c21d8e25a50b45
Gonzalo <Gonzalo@DESKTOP-76ILFO1>
2025-10-18 15:54:52 +0200

    17/10/25

--- src/elseif/Ejercicio2.java
+++ src/elseif/Ejercicio2.java
@@ -1,10 +1,33 @@
 package elseif;
 
+import java.util.Scanner;
+
+import java.util.Scanner;
+
 public class Ejercicio2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+				
+	//Un int para la edad
+	int edad;
+				
+	//Creo un escaner para guardar los valores
+	Scanner reader = new Scanner (System.in);
+				
 
-	}
-
+	//Ask the question and read the variable.
+	System.out.println("Introduzca su edad: ");
+	
+	//Leo la edad
+	edad = reader.nextInt();
+				
+				//Set the conditional.
+				if (edad >= 18) {
+					System.out.println("Eres mayor de edad.");
+				}
+				
+				//Close the scanner
+				reader.close();
 }
+}
+
